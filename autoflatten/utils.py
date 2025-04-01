cb0f2ae0a1deadc77502a347784a997a20fe8d18
Matteo Visconti di Oleggio Castello <mvdoc@berkeley.edu>
2025-04-01 07:16:00 -0700

    FIX deal with numpy arrays

--- autoflatten/utils.py
+++ autoflatten/utils.py
@@ -2,6 +2,8 @@
 
 import json
 
+import numpy as np
+
 
 def load_json(fn):
     """
@@ -15,6 +17,18 @@ def load_json(fn):
 def save_json(fn, data, indent=None):
     """
     Save a dictionary to a json file.
+    Handles numpy arrays by converting them to lists.
     """
+
+    class NumpyEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, np.ndarray):
+                return obj.tolist()
+            if isinstance(obj, np.integer):
+                return int(obj)
+            if isinstance(obj, np.floating):
+                return float(obj)
+            return super().default(obj)
+
     with open(fn, "w") as f:
-        json.dump(data, f, indent=indent)
+        json.dump(data, f, indent=indent, cls=NumpyEncoder)
